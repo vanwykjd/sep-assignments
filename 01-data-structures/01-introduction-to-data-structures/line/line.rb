836e819d92575cca863230f2e4f575d6a1a6f9cd
Josh Van Wyk <joshvanwyk@ymail.com>
2018-05-17 15:36:01 -0400

    01 SEP assignment answers

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -9,26 +9,35 @@ class Line
   end
 
   def join(person)
+    self.members.push(person)
   end
 
   def leave(person)
+    index(person) ? self.members.delete(person) : nil
   end
 
   def front
+    self.members[0]
   end
 
   def middle
+    middle_index = (self.members.length / 2).floor
+    self.members[middle_index]
   end
 
   def back
+    self.members[self.members.length - 1]
   end
 
   def search(person)
+    index(person)
   end
 
   private
-
+  
   def index(person)
+    index = self.members.index(person)
+    index ? self.members[index] : nil
   end
 
 end
