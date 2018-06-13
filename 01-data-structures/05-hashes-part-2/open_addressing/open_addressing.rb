00aa6bd2cd013eaf91f21fdb94c3643a2a3e62ae
Josh Van Wyk <joshvanwyk@ymail.com>
2018-06-13 19:23:08 -0400

    05 SEP assignment answers

--- 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
+++ 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
@@ -2,29 +2,66 @@ require_relative 'node'
 
 class OpenAddressing
   def initialize(size)
+    @items = Array.new(size)
   end
 
   def []=(key, value)
+    hash = Node.new(key, value)
+    hash_code = index(hash.key, size)
+    if @items[hash_code] != nil
+      hash_code = next_open_index(hash_code)
+      if hash_code < 0
+        resize 
+        @items[hash_code] = hash
+      end 
+    end
+    @items[hash_code] = hash
   end
 
   def [](key)
+    hash_code = index(key, size)
+    hash = @items[hash_code]
+    while hash.key != key && hash != nil
+      hash_code += 1
+      hash = @items[hash_code % size]
+    end
+    hash.value
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    ascii_val = key.each_byte.sum
+    true_index = ascii_val % size
+    true_index
   end
 
   # Given an index, find the next open index in @items
   def next_open_index(index)
-  end
+    i = index
+    while @items[i % size] != nil && i < (index + size)
+      i += 1
+    end
+    if i < (index + size)
+      i % size
+    else 
+      index - i
+    end
+  end 
 
   # Simple method to return the number of items in the hash
   def size
+    @items.length
   end
 
   # Resize the hash
   def resize
+    temp = @items
+    @items = Array.new(size * 2)
+    temp.each do |hash|
+      hash == nil ? next : @items[index(hash.key, size)] = hash
+    end
   end
+  
 end
--- 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
@@ -6,25 +6,100 @@ class LinkedList
 
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
+    if empty?
+      @head = node
+      @tail = node
+    else 
+      temp = @head
+      while temp.next != nil
+        temp = temp.next
+      end
+      temp.next = node
+      @tail = node
+    end
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
+    temp = @head
+    if @head == @tail
+       @tail = nil
+       @head = nil
+    else
+      while temp.next != @tail
+        temp = temp.next
+      end
+      temp.next = temp.next.next
+      @tail = temp
+    end
   end
 
   # This method prints out a representation of the list.
   def print
+    nodes = []
+    if empty?
+      puts "Nothing has been added to list to print."
+    else
+      temp = @head
+      while temp.next != nil
+        nodes << temp.data
+        temp = temp.next
+      end
+      nodes << temp.data
+      puts nodes 
+    end
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+    if empty?
+      puts "Nothing has been added to list to delete."
+    else
+      temp = @head
+      if @head == node
+        remove_front
+      elsif @tail == node
+        remove_tail
+      else
+        while temp.next != node
+          if temp.next
+            temp = temp.next
+          else 
+            puts "Node does not exist in list"
+            break
+          end
+        end
+        unless temp.next == nil
+          temp.next = temp.next.next
+        end
+      end
+    end
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
+    if empty?
+      @head = node
+      @tail = node
+    else 
+      node.next = @head
+      @head = node
+    end
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
+    temp = @head
+    if @head == @tail
+       @head = nil
+       @tail = nil
+    else
+      temp = temp.next
+      @head = temp
+    end
+  end
+
+  def empty?
+    @head && @tail ? false : true
   end
 end
